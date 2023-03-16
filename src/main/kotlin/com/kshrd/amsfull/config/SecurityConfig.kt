488156aa7a6714434b4c67938b95154c8750725f
dayan <eam.dayan@gmail.com>
2023-03-16 18:13:21 +0700

    add cors policy

---
+++ src/main/kotlin/com/kshrd/amsfull/config/SecurityConfig.kt
@@ -0,0 +1,41 @@
+package com.kshrd.amsfull.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun cors(): CorsConfigurationSource {
+        val cors = CorsConfiguration()
+        cors.allowedOrigins = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.setCorsConfigurations(mapOf("/**" to cors))
+        return source
+    }
+
+    @Bean
+    fun securityConfig(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests {
+                it.anyRequest().permitAll()
+            }
+            .csrf().disable()
+            .cors()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        return http.build()
+    }
+
+}
